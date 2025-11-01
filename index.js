345ebbcee0cbd37022ed1554f134db3c523c6b18
Andreas <als0825a@gmail.com>
2025-11-01 15:55:47 +0100

    Still Working

--- index.js
+++ index.js
@@ -4,13 +4,7 @@
  const list = document.getElementById("gally");
  
 
- const API_URL = "https://api.artic.edu/api/v1/artworks?limit=10";
-
-(async () => {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-
-  const artworks = data.data;
+getGallery(artworks => {
 
   artworks.forEach((art) => {
     const li = document.createElement("li");
@@ -20,20 +14,22 @@
     list.appendChild(li);
   });
 
-  list.addEventListener("click", (e) => {
-    if (e.target.tagName === "LI") {
-      const imageId = e.target.dataset.imageId;
-      const artist = e.target.dataset.artist;
-      const title = e.target.textContent;
+    list.addEventListener("click", (e) => {
+      if (e.target.tagName === "LI") {
+        const imageId = e.target.dataset.imageId;
+        const artist = e.target.dataset.artist;
+        const title = e.target.textContent;
 
-      if (imageId) {
-        const imageUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
-        image.src = imageUrl;
-        text.textContent = `${title} by ${artist}`;
-      } else {
-        image.src = "";
-        text.textContent = "No image available.";
+          if (imageId) {
+            const imageUrl = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
+            image.src = imageUrl;
+            text.textContent = `${title} by ${artist}`;
+           } 
+           else {
+            image.src = "";
+            text.textContent = "No image available.";
+          }
       }
-    }
-  });
-})();
+    });
+  
+} )
